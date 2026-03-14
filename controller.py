d8beb263370523d011069a0090d7b5a4a14d7761
Mfon Nwa <mfonnwa@gmail.com>
2026-03-14 14:20:00 +0100

    ready to start the python implementation

---
+++ controller.py
@@ -0,0 +1,15 @@
+# check the results flow well
+
+# to do it?
+
+# 1. get the teams win, loss, draw and points
+# 2. get goal scorers
+# 3. get assists
+
+
+def calculate():
+    ...
+
+
+if __name__ == "__main__":
+    ...
