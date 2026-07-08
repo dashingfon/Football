2424678164dc281ce17885943f95e03400da00be
Mfon Nwa <mfonnwa@gmail.com>
2026-07-09 00:54:00 +0100

    about to start building the components

--- src/renderer.py
+++ src/renderer.py
@@ -1,13 +1,19 @@
 import pathlib
 
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(
+    loader=FileSystemLoader("templates")
+)
+
+template = env.get_template("base_app.jinja")
 
 if __name__ == "__main__":
 
     path = pathlib.PurePath(__file__).parent.parent
-    with open(path / "templates" / "base_app.jinja") as t:
-        raw_template = t.read()
-    template = Template(raw_template)
+    # with open(path / "templates" / "base_app.jinja") as t:
+    #     raw_template = t.read()
+    # template = Template(raw_template)
     context = {"root": "./", "raw_season": "july_august_2026"}
     rendered = template.render(**context)
 
